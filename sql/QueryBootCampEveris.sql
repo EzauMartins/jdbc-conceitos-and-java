348078e8cc7d229ba53f700caed3049bf1f1b339
Ezau Martins <ezaumateus97@gmail.com>
2021-08-11 21:39:51 -0400

    Inicial

---
+++ sql/QueryBootCampEveris.sql
@@ -0,0 +1,16 @@
+Create dataBase digital_everis_bootCamp
+
+GO
+use digital_everis_bootCamp
+
+GO
+
+Create table aluno(
+id INTEGER AUTO_INCREMENT ,
+nome VARCHAR(80) NOT NULL,
+idade INTEGER NOT NULL,
+estado CHARACTER(2) NOT NULL,
+PRIMARY KEY(id)
+);
+
+

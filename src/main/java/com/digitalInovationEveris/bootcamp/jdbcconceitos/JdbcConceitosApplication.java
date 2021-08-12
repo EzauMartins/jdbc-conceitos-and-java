9016757336a9ab76334c21131bf9750939665c93
Ezau Martins <ezaumateus97@gmail.com>
2021-08-11 21:38:16 -0400

    Initial commit

---
+++ src/main/java/com/digitalInovationEveris/bootcamp/jdbcconceitos/JdbcConceitosApplication.java
@@ -0,0 +1,13 @@
+package com.digitalInovationEveris.bootcamp.jdbcconceitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcConceitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcConceitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalInovationEveris/bootcamp/jdbcconceitos/JdbcConceitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalInovationEveris.bootcamp.jdbcconceitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcConceitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
